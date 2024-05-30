0a2b6ff02fe98cc9c52d363686005b75f8fbabea
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-05-30 19:10:04 +0900

    [Add] 플랫폼 버그 수정

--- 3D-Dungeon/Assets/Scripts/Platform/JumpPlatform.cs
+++ 3D-Dungeon/Assets/Scripts/Platform/JumpPlatform.cs
@@ -5,6 +5,13 @@ public class JumpPlatform : MonoBehaviour
     public float jumpPlatformForce;
     private void OnCollisionEnter(Collision collision)
     {
-        CharacterManager.Instance.Player.controller.Jump(jumpPlatformForce);
+        Vector3 normalVector =  collision.contacts[0].normal;
+        float value = Vector3.Dot(normalVector, Vector3.up);
+        // Vector3.Dot = º¤ÅÍ°£ ³»Àû
+        if ( Mathf.Approximately(value,-1) )
+        {
+            Debug.Log("asdf");
+            CharacterManager.Instance.Player.controller.Jump(jumpPlatformForce);
+        }        
     }
 }
